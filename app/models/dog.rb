cda34058c996746669c7767a4d3715a3c1172a1c
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-03-14 00:43:28 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,16 +1,16 @@
-class Dog 
-  attr_accessor :name, :breed, :age 
+class Dog
+  attr_accessor :name, :breed, :age
 
   @@all = []
 
   def initialize(name, breed, age)
-    @name = name 
-    @breed = breed 
-    @age = age 
-  end 
+    @name = name
+    @breed = breed
+    @age = age
+  end
 
-  def self.all 
-    @@all 
-  end 
+  def self.all
+    @@all
+  end
 
 end 
